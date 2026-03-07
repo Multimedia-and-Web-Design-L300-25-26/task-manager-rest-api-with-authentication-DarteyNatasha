31f8ee07be312b3c5cbd61351f4493555a9574f0
Dartey Natasha <darteynatasha409@gmail.com>
2026-03-07 17:49:30 +0000

    assignment

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 import dotenv from "dotenv";
 import app from "./src/app.js";
-import connectDB from "./src/config/db.js";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-// Connect to MongoDB
-connectDB();
+if (process.env.NODE_ENV !== 'test') {
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  const checkConnection = () => {
+    if (mongoose.connection.readyState === 1) {
+      console.log("MongoDB is connected. Starting server...");
+      app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+      });
+    } else {
+      console.log("Waiting for MongoDB connection...");
+      setTimeout(checkConnection, 1000);
+    }
+  };
+
+  // Start checking after 1 second
+  setTimeout(checkConnection, 1000);
+}
+
+export { app };
